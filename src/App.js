dc02ce55ef9376fe8b8b016e61445aeb4bae81e0
SHargus <87950738+harguss@users.noreply.github.com>
2023-02-15 12:05:06 -0600

    installed latest v of react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import { Carousel } from "react-bootstrap";
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "./App.css";
 // import About from './About.js';
 
@@ -34,30 +35,39 @@ class App extends React.Component {
     console.log(this.state.books);
     let books = this.state.books.map((book) => (
       <Carousel.Item key={book._id}>
-        <p>
-          {book.title} is {book.description}
-        </p>
-        <img src={`http://placekitten.com/g/200/300`} alt={'cat'}/>
+        {/* <img src={book.cover} alt="Book cover" /> */}
+        <h3>{book.title}</h3>
+          <p>{book.description}</p>
+          
+        {/* <Carousel.Caption>
+          <h3>{book.title}</h3>
+          <p>{book.description}</p>
+          <img src={`http://placekitten.com/g/200/300`} alt={'cat'} />
+        </Carousel.Caption> */}
+      
       </Carousel.Item>
     ));
     return (
       <>
-      <section>
-        <header>
-          <h1>Good Reads</h1>
-        </header>
-        <main>
+       
+          <header>Good Reads</header>
+
+          
+       
+        <main className="carousel-container">
           {this.state.books.length > 0 ? (
             <Carousel>{books}</Carousel>
           ) : (
             <p>The book collection is empty.</p>
           )}
-        </main>
-        {/* <About /> */}
-        </section>
+       </main>
+     
+          
+           {/* <About /> */}
+     
       </>
-  );
-}
+    );
+  }
 }
 
 
